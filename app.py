55c4d4e7c21705c0634a170a4bf41eff7cc7237f
DocMonster7 <34396385+DocMonster7@users.noreply.github.com>
2021-04-24 12:17:13 +0530

    added firebase connection

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import flask
 from flask import request
-
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import datetime
+from pandas import *
+import json
+import os
 
 app = flask.Flask(__name__)
+cred = credentials.Certificate('open-street-map-research-firebase-adminsdk-4kv8q-3f47cae2b0.json')
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://open-street-map-research-default-rtdb.firebaseio.com'})
 
 
 @app.route('/',methods=['GET'])
 
 def home():
-    return 'hello'
+    ref = db.reference('osm-data/analyzed/india/top_5/data')
+    return ref.get()
