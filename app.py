811f75fac76618a37728a7bfefbcf0e0e2fcd4a9
DocMonster7 <34396385+DocMonster7@users.noreply.github.com>
2021-05-01 01:04:37 +0530

    added base64 conversion of fig and return to the request

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 from flask import request
 from flask_restful import Api, Resource
+from flask_cors import CORS
 import pyrebase
 import datetime
 import pandas as pd
@@ -9,6 +10,9 @@ import os
 from helper_functions import *
 from DataRetrieve import *
 import matplotlib.pyplot as plt
+import base64
+from PIL import Image
+from io import BytesIO
 
 firebaseConfig = {
     "apiKey": "AIzaSyBTzzXFHncci7RanGMjNfduJ8_471RkYoU",
@@ -22,6 +26,7 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 store = firebase.storage()
 app = flask.Flask(__name__)
+CORS(app)
 api = Api(app)
 countries =['brazil','india','china','south africa','russia']
 category_ = ['building','leisure','amenity','office','man_made','advertising','shop','craft','historic','landuse','tourism','boundary']
@@ -88,21 +93,41 @@ class JSON_CSV(Resource):
 
                 x = Data_Manipulation(merge_df, years, category, country)
                 fig = x.RefineData_and_GenerateGraph('bar')
-                
-                file_name = 'test.svg'
-                plt.savefig(file_name,bbox_inches='tight')#, dpi=150)
+                print(type(fig))
+                file_name = 'test.png'
+                plt.savefig(file_name,bbox_inches='tight', dpi=100)
                 # store.child("test.jpg").put('test.jpg')
                 # store.child(file_name).get_url
                 # os.remove(file_name)
                 # store.child(file_name).download("download",file_name)
+            def fig2img(fig):
+                """Convert a Matplotlib figure to a PIL Image and return it"""
+                import io
+                buf = io.BytesIO()
+                fig.savefig(buf)
+                buf.seek(0)
+                img = Image.open(buf)
+                return img  
 
-
+            img = fig2img(fig)
+            # print(type(img))
             # print(dates.val())
             # print(json_dict['20140101']['frequency'])
             # df = pd.DataFrame.from_dict(json_dict,orient='index')
             # df.reset_index(level=0,inplace=True)
             # print(df)
-            return "Bro sent okay chill"
+            # with open(img, 'rb') as binary_file:
+            #     binary_file_data = binary_file.read()
+            #     base64_encoded_data = base64.b64encode(fig)
+            #     base64_message = base64_encoded_data.decode('utf-8')
+            
+            output_buffer = BytesIO()
+            img.save(output_buffer, format='png')
+            binary_data = output_buffer.getvalue()
+            base64_data = base64.b64encode(binary_data)
+            base64_message = base64_data.decode('utf-8')
+
+            return base64_message
         return "Invalid input"
 
 
